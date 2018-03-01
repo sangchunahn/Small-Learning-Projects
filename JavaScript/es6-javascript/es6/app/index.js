3a66278373af584dde1c8ca2e04a6bcbd42f8a6d
Sang-Chun Ahn <sangchunahn@yahoo.com>
2018-03-01 11:07:38 -0700

    added notes about es6

--- JavaScript/es6-javascript/es6/app/index.js
+++ JavaScript/es6-javascript/es6/app/index.js
@@ -8,5 +8,19 @@
 // ex. `hello ${something}`
 
 const limit = 100;
+// const prevents it from being changed
 // limit = 200;
 console.log(limit)
+
+//spread operator in es6
+// let a  = [20, 30, 40]
+// let b = [10, ...a, 50]
+
+let arrow = () => {
+  console.log('arrow function in es06')
+}
+
+// map and filter are parts of es6
+// "string".repeat(n) repeats the string a number of times 
+// startsWith or EndsWith returns a boolean to see if the string starts with or ends with an input
+// includes is a better method
